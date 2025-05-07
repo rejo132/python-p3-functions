4fadb21e2a077e1efb274529c58189275ae037d4
rejo132 <enoch.luke1@student.moringaschool.com>
2025-05-07 20:02:40 +0300

    completes lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default()   
 
 def add(num1, num2):
-    pass
+    print(num1 + num2)
+    return 45 + 55
 
 def halve(number):
-    pass
+    result = number / 2
+    print(result)
+    return result
+
+
+
